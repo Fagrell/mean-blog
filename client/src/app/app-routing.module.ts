447138ac006ac809994ec1b1bd3a74e76e184ab0
Alexander Fagrell <fagrell@outlook.com>
2018-05-06 18:22:08 +0100

    Added home component as well as hooking up the routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+  { 
+    path: '', 
+    component: HomeComponent 
+  },
+  { 
+    path: '**', 
+    component: HomeComponent 
+  }
+];
 
 @NgModule({
   declarations: [],
-  imports: [],
+  imports: [ 
+      RouterModule.forRoot(
+        appRoutes,
+        { enableTracing: true } // <-- debugging purposes only
+      )
+  ],
   providers: [],
-  bootstrap: []
+  bootstrap: [],
+  exports: [RouterModule]
 })
 
 export class AppRoutingModule { }
