eae71567bb9f11da1a80a019c76101e7e4831e36
Alexander Fagrell <fagrell@outlook.com>
2018-05-07 20:20:06 +0100

    Added about information

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { AboutComponent } from './components/about/about.component';
 
 const appRoutes: Routes = [
   { 
     path: '', 
     component: HomeComponent 
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   },
   { 
     path: '**', 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
