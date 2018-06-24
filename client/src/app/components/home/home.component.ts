52c4f3c136aa686224fdb458b8050bb786e35940
Alexander Fagrell <fagrell@outlook.com>
2018-06-24 22:26:44 +0100

    Worked on the home page. Basically copied the data over from the blog page. The blog page will be instead used when reading a specific blog.

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+import { BlogService } from '../../services/blog.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  message: String = "";
+  messageClass: String = "";
+  loadingFeed: Boolean  = false;
+  blogs;
 
-  ngOnInit() {
+  public blogMessage: String = "";
+
+  constructor(
+    private router: Router,
+    private blog: BlogService
+  ) {}
+
+  newBlogForm() {
+    this.router.navigate(['/blog-edit']);
   }
 
+  refreshFeed() {
+    this.loadingFeed = true;
+    setTimeout(() => {
+      this.loadingFeed = false;
+    }, 4000)
+  }
+
+  ngOnInit() {
+    this.blog.allBlogs().subscribe(data => {
+      console.log("GOES HERE?");
+      if(!data['success']) {
+        return console.log("Failed getting all blogs, becase: " + data['message']);
+      }
+      console.log("goes here?")
+      this.blogs = data['blogs'];
+    });
+  }
 }
