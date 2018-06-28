dd35e4b6e10202c68d30475ab1c06fbb7f4f0e2c
Alexander Fagrell <fagrell@outlook.com>
2018-06-28 23:55:09 +0100

    Several updates in this commit. Polished the about page a bit further. It's now possible to navigate from the home page to the different titles in the about page. Also it's now possible to read a whole blog (and not just the summary).

--- client/src/app/components/about/about.component.ts
+++ client/src/app/components/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router'
+
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  doScroll(title: string) {
+    try {
+      const elements = document.getElementById(title);
+      elements.scrollIntoView();
+      window.scrollBy(0, -80);
+    } finally {
+
+    }
+  }
 
   ngOnInit() {
+    const title = this.activatedRoute.snapshot.queryParams['title'];
+    if (!title) {
+      return;
+    }
+    this.doScroll(title);
   }
 
 }
