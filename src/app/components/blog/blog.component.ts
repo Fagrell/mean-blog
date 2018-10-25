97209d974773ff5c2528ddacfe17a20683c3cfeb
Alexander Fagrell <fagrell@outlook.com>
2018-10-25 12:46:32 +0100

    Blog is now properly generated on the server

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -2,8 +2,7 @@ import { WINDOW } from '@ng-toolkit/universal';
 import { Component, OnInit , Inject} from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { BlogService } from '../../services/blog.service';
-import { TransferState, makeStateKey } from '@angular/platform-browser';
-import { Title, Meta } from '@angular/platform-browser';
+import { TransferState, makeStateKey, Title, Meta } from '@angular/platform-browser';
 
 const STATE_KEY_TITLE = makeStateKey('title');
 const STATE_KEY_BODY = makeStateKey('body');
@@ -33,7 +32,7 @@ export class BlogComponent implements OnInit {
     private blog: BlogService,
     private state: TransferState,
     private pageTitle: Title,
-    private meta: Meta,
+    private meta: Meta
   ) {}
 
   newBlogForm() {
@@ -50,23 +49,33 @@ export class BlogComponent implements OnInit {
   }
 
   updateDescription() {
-    const tags = this.tags.join(' ');
+    const tags = this.tags.join(', ');
     const description = 'Blog post covering: ' + tags;
     this.meta.updateTag({
-      name: 'description',
+      property: 'description',
       content: description
     });
     this.meta.updateTag({
-      name: 'og:title',
+      property: 'og:title',
       content: this.title.toString()
     });
     this.meta.updateTag({
-      name: 'og:description',
+      property: 'og:description',
       content: description
     });
+
+    this.meta.updateTag({
+      property: 'og:author',
+      content: this.createdBy.toString()
+    });
+
   }
 
-  getBlog(title ) {
+  getHref(title) {
+    return 'https://www.cleanqt.io/blog/' + this.route.snapshot.url[1];
+  }
+
+  getBlog(title) {
     this.blog.oneBlog(title).subscribe(data => {
       if(!data['success']) {
         this.router.navigate(['/home'], { skipLocationChange: true });
@@ -89,9 +98,16 @@ export class BlogComponent implements OnInit {
       this.tags = data['blog'].tags;
       this.state.set(STATE_KEY_TAGS, this.tags as Array<string>);
 
+      
+      this.href = this.getHref(title);
+      this.state.set(STATE_KEY_HREF, this.href as string);
       this.updatedSEOData();
-      this.window.scroll(0,0);
 
+      if (!this.window) {
+        return;
+      }
+      this.window.scroll(0,0);
+      this.shareHref = this.window.location.href;
     });
   }
 
@@ -104,20 +120,19 @@ export class BlogComponent implements OnInit {
     this.href = this.state.get(STATE_KEY_HREF, null as string);
 
     this.route.params.subscribe(params => {
+
       const title = params['title'];
       if (!title) {
         this.router.navigate(['/home'], { skipLocationChange: true });
         return;
       }
 
-      this.shareHref = this.window.location.href;
-      const href = this.window.location.origin + '/blog' + title;
+      const href = this.getHref(title);
       if (!this.body || href != this.href) {
         this.getBlog(title);
-        this.href = href;
-        this.state.set(STATE_KEY_HREF, this.href as string);
         return;
       }
+      
       this.updatedSEOData();
 
     });
