03833b6b9cea3c802c6d2e6bf3bdd0fcddb85f55
Alexander Fagrell <fagrell@outlook.com>
2018-10-25 12:41:45 +0100

    Updated meta tags on the about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,7 +1,7 @@
 import { WINDOW } from '@ng-toolkit/universal';
 import { Component, OnInit , Inject} from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router'
-
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
@@ -11,7 +11,9 @@ import { Router, ActivatedRoute } from '@angular/router'
 export class AboutComponent implements OnInit {
 
   constructor(@Inject(WINDOW) private window: Window, 
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private pageTitle: Title,
+    private meta: Meta
   ) { }
 
   doScroll(title: string, offset: number) {
@@ -25,6 +27,21 @@ export class AboutComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.pageTitle.setTitle('About / Clean Qt');
+    const description = "My name is Alexander Fagrell and I'm a software developer and have several years of experience using modern C++ and Qt. With this blog, I aim to deepen my knowledge by exploring the ins and outs of the Qt framework and to share my findings and conclusions";
+    this.meta.updateTag({
+      property: 'description',
+      content: description
+    });
+    this.meta.updateTag({
+      property: 'og:title',
+      content: 'About the blog and the author'
+    });
+    this.meta.updateTag({
+      property: 'og:description',
+      content: description
+    });
+
     const title = this.activatedRoute.snapshot.queryParams['title'];
     if (!title) {
       return;
